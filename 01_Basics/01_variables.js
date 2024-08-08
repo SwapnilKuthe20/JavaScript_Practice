cba32ef3ddc0b65334732268d5bbe8ee4275b186
SwapnilKuthe20 <swapnilkuthe20@gmail.com>
2024-08-08 12:10:07 +0000

    variables in js

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,25 @@
+const accountId = 123456;
+let accountName = "Swapnil Kuthe";
+var accountEmail = "swapnilkuthe20@gmail.com";
+accountCity = "Nagpur";
+
+
+// console.log(accountId);
+
+console.table([accountId, accountName, accountEmail, accountCity])
+// accountId = 654321;  // TypeError: Assignment to constant variable.
+// const can't be reAssign or reDefined
+// let accountId = 756756; //SyntaxError: Identifier 'accountId' has already been declared
+// console.log(accountId);
+
+accountName = "Swap";
+accountEmail = "Swap@gmail.com";
+accountCity = "Bhandara";
+
+
+console.table([accountId, accountName, accountEmail, accountCity])
+
+
+
+
+
