efcc1fedb7ad392e3b33ec6c60a3a033d3f2b7c6
SwapnilKuthe20 <swapnilkuthe20@gmail.com>
2024-09-26 15:25:17 +0530

    basic js practice

---
+++ 01_Basics/08_arrayMethods.js
@@ -0,0 +1,17 @@
+
+let arr = [10, 20, 30, 40, 50, 60];
+// console.log(arr);                        // [10, 20, 30, 40, 50, 60]
+// console.log(arr.length);                 // 6
+// console.log(typeof arr);                 // object
+// console.log(Array.isArray(arr));         // true
+
+
+
+
+
+
+
+
+
+
+
